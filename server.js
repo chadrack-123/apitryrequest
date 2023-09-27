7b303effdacb82206dbb69d36cf358b70abe9326
chadrack-123 <chadrackmulamba2@gmail.com>
2023-09-27 15:05:16 +0200

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,16 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Define your webhook endpoint
-app.post('/webhook-endpoint', (req, res) => {
-  // Handle the incoming data from the webhook here
-  const payload = req.body;
-  console.log('Received webhook data:', payload);
+// app.post('/webhook-endpoint', (req, res) => {
+//   // Handle the incoming data from the webhook here
+//   const payload = req.body;
+//   console.log('Received webhook data:', payload);
 
-  // Respond to the webhook request
-  res.status(200).json({ message: 'Webhook data received successfully' });
-});
+//   // Respond to the webhook request
+//   res.status(200).json({ message: 'Webhook data received successfully' });
+// });
+
+app.use("/api/services", require("./routes/servicesRoutes"))
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+
+  console.log("To comfirm that is running")
 });
